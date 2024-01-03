3c0a4491e69315744b29b0275420c98f97180446
brian d foy <briandfoy@pobox.com>
2024-01-03 13:29:31 -0500

    update copyright

--- lib/Tie/BoundedInteger.pm
+++ lib/Tie/BoundedInteger.pm
@@ -71,7 +71,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2005-2023, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2005-2024, brian d foy <bdfoy@cpan.org>. All rights reserved.
 This software is available under the terms of the Artistic License 2.0.
 
 
