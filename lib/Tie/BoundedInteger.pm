12b1eef0a27acae1b55cb384d993f19e686c6e8c
brian d foy <briandfoy@pobox.com>
2024-01-17 13:05:33 -0500

    Update email address

--- lib/Tie/BoundedInteger.pm
+++ lib/Tie/BoundedInteger.pm
@@ -71,7 +71,7 @@ brian d foy, C<< <briandfoy@pobox.com> >>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright © 2005-2024, brian d foy <bdfoy@cpan.org>. All rights reserved.
+Copyright © 2005-2024, brian d foy <briandfoy@pobox.com>. All rights reserved.
 This software is available under the terms of the Artistic License 2.0.
 
 
